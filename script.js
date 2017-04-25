0139e990405a4121a3f5400a5d5be2f52626c77a
Chelsea Weber <chelseaweber92@gmail.com>
2017-04-25 16:48:03 -0400

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	
+	
+	$("#submit-btn").click(function(){
+		var firstName = $("#first-name").val();
+		console.log(firstName);
+		var lastName = $('#last-name').val();
+		var phone = $('')
+	})
+	
+	
+});
