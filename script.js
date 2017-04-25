5e9afc4b54c2f2199416324f234287696afa95fc
nathan-rabens <nathanrabens@gmail.com>
2017-04-25 16:47:23 -0400

    dk

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  var obj = {
+      firstName: "nathan",
+      lastName: "rabens",
+      phoneNumber: "3333333333",
+      email: "nanana@gjght",
+      request: "tnfdndndndndndndn"
+  }
+
+var firstName = $("#first-name");
+var lastName = $("#last-name");
+var phoneNumber = $("#phone");
+var email = $("#email");
+var request = $("#request").val();
+
+function submit () {
+  onclick.value
+
+}
+
+console.log($("#request").attr("val"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
