c82341972529897f2e16adc40fbe2dd26533ac0e
Chelsea Weber <chelseaweber92@gmail.com>
2017-04-26 12:37:46 -0400

    clean up script code

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
+	//drop-down seating area
+	$('.seatPicker').click(function(){
+		$('#seating-area').toggle('slow');
+	});
 
-	//////////create empty array for seats with loop (1 to 24) CHELSEA
+	//drop down reservation form
+	$('#seating-area').click(function(){
+		$('#reservation-form').toggle('slow');
 	
-	//delcare empty seats array
+	})
+	
 	var seats = [];
-	//declare empty seat var
-	var seat 
+	
+	var seat;
+	
+	var selectedSeat;
+		
 	//create function to push 24 seats, with props, into seats array
 	function addSeats() {
 		for (i=1; i<25; i++) {	
@@ -23,73 +33,26 @@ $(document).ready(function(){
 	
 	addSeats();
 	
-	console.log(seats);
-	
-	//push seats with empty object properties into the array ((name.val, seat number.text, phone.val, email.val, request.val)) CHELSEA
-	
+	//change color of selected seat, assign selectedSeat value
+	$(".seat").click(function(e){
+		$(this).text("Reserve a Seat!").css("background-color", "#c6ff6b");
+		selectedSeat = e.target.innerText;
+		console.log(e.target.innerText);
+
+	});
+
 	//function to add user info to selected seat
-	function addInfo() {
-		//var for selectedSeat = 
-		var selectedSeat = $('.selected p').text();
+	$('#submit-btn').click(function() {
 		console.log(selectedSeat);
-		selectedSeat = parseInt(selectedSeat);
-		console.log(selectedSeat);
+	//		selectedSeat = parseInt(selectedSeat);
+	//		console.log(selectedSeat);
 		seats.forEach(function(seat){
-			if (selectedSeat === seat.number) {
+			if (selectedSeat == seat.number) {
 				seat.firstName = "Chelsea";
-				seat.lastName = document.getElementById('last-name').value;
-//				seat.lastName = $('#last-name');
 			}
 		})
-	}
-	
-	$('#submit-btn').click(addInfo(this));
-	
-	console.log(seats);
-//	console.log(seats);
-//	$("#submit-btn").click(addInfo(event.target));
-//	
-//	console.log(seats);
-	
-	//if clicked add class selected to seat Karen
-
-	//if seat is clicked form drops down NATHAN
-
-	//if form submited, seats with class selected toggleClass to researved, add innerText = seat.name Karen
-  // $(#book) function(){
-  //   if {
-  //
-  //   }
-  // };
-
-	//create empty array for seats with loop (1 to 24)
-
-	//push seats with empty object properties into the array ((name.val, seat number.text, phone.val, email.val, request.val))
-
-	//if clicked add class selected to seat
-
-	//if seat is clicked form drops down
-
-	//if form submited, seats with class selected toggleClass to researved, add innerText = seat.name
-
-
-	//add button that drops down seating-area div if clicked
-
-//hover over seat to change color (check availability)
-	
-//when click seat will say which seat
-  $(".seat").click(function(){
-        $(this).text("Reserve a Seat!").css("background-color", "#c6ff6b");
-
-    });
-
-$('.seatPicker').click(function(){
-  $('#seating-area').toggle('slow');
-});
-
-$('#seating-area').click(function(){
-  $('#reservation-form').toggle('slow');
-})
+		console.log(seats);
+	});
 
 });
 
