3f7d19a5f32a262d568f32f47a27a08478c0e1c5
kkhalifah <karen.khalifah@gmail.com>
2017-04-25 21:44:35 -0400

    adjusted form controls

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ $(document).ready(function(){
   //       $(this).css("background-color", "#0ebaa1");
   //  });
   $(".seat").click(function(){
-        $(this).text("Reserve a Seat!");
+        $(this).text("Reserve a Seat!").css("background-color #0ebaa1");
     });
