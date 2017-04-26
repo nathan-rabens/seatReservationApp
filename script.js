92f61ab288db8e9a1d68111ba3b3107ac1ed0e09
kkhalifah <karen.khalifah@gmail.com>
2017-04-25 20:33:06 -0400

    added some stying

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+
 	$("#submit-btn").click(function(){
 		firstName = $("#first-name").val();
 		lastName = $("#last-name").val();
@@ -9,6 +9,14 @@ $(document).ready(function(){
 	});
 
 	console.log(firstName);
-	
-});
 
+});
+//pick best look
+//hover over seat to change color (check availability)
+    $(".seat").hover(function(){
+       $(this).css("background-color", "#c6ff6b");
+      }, function(){
+        $(this).css("background-color", "#0ebaa1");
+   });
+// mouseover to show text underneath
+   //  .fadeTo(700, 0.25); should we have it fade to show unavail text
