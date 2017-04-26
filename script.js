b076ffccb0d9cf79970bf4474210f1dc7c736ef0
kkhalifah <karen.khalifah@gmail.com>
2017-04-25 21:15:07 -0400

    styling the page

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ $(document).ready(function(){
 	console.log(firstName);
 
 });
-//pick best look
 //hover over seat to change color (check availability)
-    $(".seat").hover(function(){
-       $(this).css("background-color", "#c6ff6b");
-      }, function(){
-        $(this).css("background-color", "#0ebaa1");
-   });
-// mouseover to show text underneath
-   //  .fadeTo(700, 0.25); should we have it fade to show unavail text
+// or mouseover to show text underneath- "reserve a seat"
+  //   $(".seat").hover(function(){
+  //      $(this).css("background-color", "#c6ff6b");
+  //     }, function(){
+  //       $(this).css("background-color", "#0ebaa1");
+  //  });
+  $(".seat").click(function(){
+        $(this).text("Reserve a Seat!");
+    });
