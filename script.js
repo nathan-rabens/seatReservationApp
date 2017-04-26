b2416994943e8dc654efbb061b20c8a3fa7a2fc1
nathan-rabens <nathanrabens@gmail.com>
2017-04-26 11:22:18 -0400

    ddd

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
 	//create empty array for seats with loop (1 to 24) CHELSEA
-	
+
 	var seats = [];
 	var seat
-	
+
 	function addSeats() {
-		for (i=1; i<25; i++) {	
+		for (i=1; i<25; i++) {
 			seat = {
 				number: i,
 				firstName: "",
 				lastName: "",
 				phoneNumber: "",
 				email: "",
-				request: "",	
+				request: "",
 			}
 			seats.push(seat);
-		} 
+		}
 	}
-	
+
 	addSeats();
-	
+
 	console.log(seats);
-	
+
 	//push seats with empty object properties into the array ((name.val, seat number.text, phone.val, email.val, request.val)) CHELSEA
-		
+
 	function addInfo() {
 		var selectedSeat = $('.selected p').text();
 		console.log(selectedSeat);
@@ -38,16 +38,16 @@ $(document).ready(function(){
 //			seat.phoneNumber = $("#phone").val();
 			seat.phoneNumber = '2483455519';
 //			seat.email = $("#email").val();
-//			seat.request = $("#request").val();		
+//			seat.request = $("#request").val();
 		}
 		console.log(seats);
 	}
-	
+
 	addInfo();
 //	$("#submit-btn").click(addInfo(event.target));
-//	
+//
 //	console.log(seats);
-	
+
 	//if clicked add class selected to seat Karen
 
 	//if seat is clicked form drops down NATHAN
@@ -73,7 +73,7 @@ $(document).ready(function(){
 	//add button that drops down seating-area div if clicked
 
 //hover over seat to change color (check availability)
-	
+
 //when click seat will say which seat
   $(".seat").click(function(){
         $(this).text("Reserve a Seat!").css("background-color", "#c6ff6b");
@@ -89,4 +89,3 @@ $('#seating-area').click(function(){
 })
 
 });
-
