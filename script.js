f880aab66f5ca1db113acac57b39393fd44dd527
Chelsea Weber <chelseaweber92@gmail.com>
2017-04-27 12:13:15 -0400

    garbage code

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
 	})
 
 	var seats = [];
+	
+	var seats2 = [];
 
 	var seat;
 
@@ -50,40 +52,88 @@ $(document).ready(function(){
 
 		selectedSeat = e.target.innerText;
 
+
 	});
 
 	//function to add user info to selected seat
 	$('#submit-btn').click(function() {
-		seats.forEach(function(seat) {
-			if (selectedSeat == seat.number) {
-				seat.firstName = $('#first-name').val();
-				seat.lastName = $('#last-name').val();
-				seat.phoneNumber = $('#phone').val();
-				seat.email = $('#email').val();
-				seat.request = $('#request').val(); 
-				guestName.attr('data-lastname', seat.lastName)
-			}
-		})
+		$('div').each(function(i) {
+			if $(i).hasClass('selected') {
+					i = {
+						firstName: $('#first-name').val(),
+						lastName: $('#last-name').val(),
+						phoneNumber: $('#phone').val(),
+						email: $('#email').val(),
+						request: $('#request').val(),
+//						attr('data-lastname', seat.lastName)
+					}
+				seats2.push(i);
+					})
+				});
 		if ($('div').hasClass('selected')) {
 			$('.selected').removeClass('selected').addClass('reserved');
-			$('.reserved p').text('Reserved');
-		}
+			$('.reserved p').text('Reserved');	
+			}
 		
-//		$('.reserved').attr('data-lastname', 'test');
-//		console.log(data-lastname);
-		console.log(guestName);
-
-	});
+		
+		
+	})
+////		seats.forEach(function(seat) {
+//			if ( $('div').each(function(i) {
+//				if ($(this).hasClas('selected') {
+//					seat = {
+//						firstName: $('#first-name').val(),
+//						lastName: $('#last-name').val(),
+//						phoneNumber: $('#phone').val(),
+//						email: $('#email').val(),
+//						request: $('#request').val(),
+////						attr('data-lastname', seat.lastName)
+//					}
+//				seats2.push(seat);
+//					})
+//				});
+////				
+////			}).hasClass('selected') ) {
+////					seat = {
+////						firstName: $('#first-name').val(),
+////						lastName: $('#last-name').val(),
+////						phoneNumber: $('#phone').val(),
+////						email: $('#email').val(),
+////						request: $('#request').val(),
+//////						attr('data-lastname', seat.lastName)
+////				}
+////				seats2.push(seat);
+////			}
+//				console.log(seats2);
+//				
+////				{
+////				seat.firstName = $('#first-name').val();
+////				seat.lastName = $('#last-name').val();
+////				seat.phoneNumber = $('#phone').val();
+////				seat.email = $('#email').val();
+////				seat.request = $('#request').val(); 
+////				seat.attr('data-lastname', seat.lastName)
+////			}
+//			if ($('div').hasClass('selected')) {
+//			$('.selected').removeClass('selected').addClass('reserved');
+//			$('.reserved p').text('Reserved');	
+//			}
+//
+//	
+////		if ($('div').hasClass('selected')) {
+////			$('.selected').removeClass('selected').addClass('reserved');
+//////			$('.reserved p').text('Reserved');
+//////		})
+////		
+//////		$('.reserved').attr('data-lastname', 'test');
+//////		console.log(data-lastname);
+////		console.log(selectedSeat);
+////
+////	});
 
 	
 	//add last name when reserved seat is hovered
 	$('.seat').mouseover(function() {
-		seats.forEach(function() {
-			if (selectedSeat == seat.number) {
-//				guestName =  this.lastName;
-//				console.log(guestName);
-			}
-		})
 		if ( $(this).hasClass('reserved') ) {
 			name = $(this).attr('data-lastname');
 			$(this).children('p').text(name);
